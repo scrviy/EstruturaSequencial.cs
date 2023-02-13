fdd4b5c408caf4b3a0bc0cc7da708ba016d55a11
scrviy <91909966+scrviy@users.noreply.github.com>
2023-02-13 12:13:32 -0300

    Estrutura Sequencial

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace EstruturaSequencial
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            double largura, comprimento, precoMetroQuadrado, area, preco;
+
+            largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            comprimento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            precoMetroQuadrado = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            area = largura * comprimento;
+            preco = area * precoMetroQuadrado;
+
+            Console.WriteLine($"Área = {area.ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"Preço = {preco.ToString("F2", CultureInfo.InvariantCulture)}");
+
+        }
+    }
+}
